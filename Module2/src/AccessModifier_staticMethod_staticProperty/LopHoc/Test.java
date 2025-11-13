6aa3b70c33c7a92f35af69bedcc6bb7d0437677a
ToanNguyen-exe <nguyenductoan.vtacl09@gmail.com>
2025-11-13 14:33:44 +0700

    upd

---
+++ Module2/src/AccessModifier_staticMethod_staticProperty/LopHoc/Test.java
@@ -0,0 +1,13 @@
+package AccessModifier_staticMethod_staticProperty.LopHoc;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Alice");
+        student.setClasses("C03");
+        System.out.println("Name: " + student.getName());
+        System.out.println("Class: " + student.getClasses());
+
+    }
+
+}

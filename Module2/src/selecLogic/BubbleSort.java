7c6894a290923442019ae604bcfce4e7fc3fa604
NDToanaz <mcmap2k6@gmail.com>
2025-11-27 14:32:23 +0700

    upd

--- Module2/src/selecLogic/BubbleSort.java
+++ Module2/src/selecLogic/BubbleSort.java
@@ -1,4 +1,28 @@
 package selecLogic;
 
 public class BubbleSort {
+    static int[] arr = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+
+    public static void bubbleSort(int[] arr) {
+        boolean needNextPass = true;
+        for (int k = 1; k < arr.length && needNextPass; k++) {
+            needNextPass = false;
+            for (int i = 0; i < arr.length - k; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    needNextPass = true;
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        bubbleSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
 }
